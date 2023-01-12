b1577baf187fa6eb204770731f36440814455c19
bajrayejoon <binaya.bajrashakya@gmail.com>
2023-01-12 18:09:51 +0530

    v-bind directives, attributes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  1. dynamic value // partial binding -->
+  <h1>{{ greet }} {{ name }}</h1>
+
+  <!--  2. text binding using directives -->
+  <!-- v-text overrides the value inside the element // full binding-->
+  <!-- <div v-text="channel"></div> -->
+
+  <!--  3. binding html // vulnerable to xss -->
+  <div v-html="channel"></div>
+
+  <!-- 4 .binding attributes / cannot use {{  }} -->
+
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Disabled Button</button>
+
+  <!--  5. binding classes -->
+  <h3 class="underline">Underlined text</h3>
+  <h2 class="underline" v-bind:class="status">Danger Status</h2>
+
+  <h3 v-bind:class="isPromote && 'promote'">Promoted</h3>
+
+  <h3 v-bind:class="isSold ? 'sold' : 'new'">Sold Out (conditional render)</h3>
+
+  <!-- 6. binding in arrays -->
+  <h2 v-bind:class="['new', 'promote']">New Movie (array class attached )</h2>
+
+  <h2 v-bind:class="[isPromote && 'promote', isSold ? 'sold' : 'new']">
+    New Movie (Conditional + ternary) !SOLD
+  </h2>
+
+  <!-- 7. binding in objects -->
+  <h2
+    v-bind:class="{
+      promote: isPromote,
+      new: !isSold,
+      sold: isSold,
+    }"
+  >
+    Object Movies SOLD
+  </h2>
+
+  <!--  8. binding styles -->
+  <h2
+    v-bind:style="{
+      color: highlightColor,
+      'font-size': fSize + 'px',
+      padding: '20px',
+    }"
+  >
+    Inline style
+  </h2>
+
+  <h2 v-bind:style="headerStyleObject">Object Style</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      name: "binaya",
+      greet: "hello",
+      channel: "<b>bajra</b>",
+      headingId: "heading",
+      isDisabled: true,
+      status: "danger",
+      isPromote: false,
+      isSold: false,
+      highlightColor: "orange",
+      fSize: 40,
+      headerStyleObject: {
+        color: "blue",
+        fontSize: "50px",
+        padding: "30px",
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +87,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promote {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold {
+  color: red;
 }
 </style>
